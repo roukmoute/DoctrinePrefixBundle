96f4dd8772ba8326e7826e79c29720fa8b23cd5c
Mathias Strasser <mstrasser@universcine.com>
2014-09-30 10:20:58 +0200

    Move sources in src directory

--- src/DependencyInjection/RoukmouteDoctrinePrefixExtension.php
+++ src/DependencyInjection/RoukmouteDoctrinePrefixExtension.php
@@ -14,7 +14,7 @@ class RoukmouteDoctrinePrefixExtension extends Extension
         $configuration = new Configuration();
         $config = $this->processConfiguration($configuration, $configs);
 
-        $loader = new Loader\XmlFileLoader($container, new FileLocator(__DIR__.'/../Resources/config'));
+        $loader = new Loader\XmlFileLoader($container, new FileLocator(__DIR__.'/../../Resources/config'));
         $loader->load('services.xml');
 
         $container->setParameter('roukmoute_doctrineprefixbundle.prefix', $config['prefix']);
